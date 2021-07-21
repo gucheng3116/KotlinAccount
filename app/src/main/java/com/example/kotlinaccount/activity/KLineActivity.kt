9dd1aa2bb31a5de0152085ee5eb53e5a8cd68405
gucheng <459843397@qq.com>
2021-07-21 11:23:22 +0800

    import resource

---
+++ app/src/main/java/com/example/kotlinaccount/activity/KLineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinaccount.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinaccount.R
+
+class KLineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kline)
+    }
+}
