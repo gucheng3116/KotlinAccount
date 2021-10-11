23794f4e20ebfe5d6547e1e4a47f962d57ecf92e
gucheng <459843397@qq.com>
2021-10-11 22:47:16 +0800

    增加change log数据库

--- app/src/main/java/com/gucheng/statistichelper/database/AccountDatabase.kt
+++ app/src/main/java/com/gucheng/statistichelper/database/AccountDatabase.kt
@@ -9,28 +9,45 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import com.gucheng.statistichelper.database.dao.DailyReportDao
 import com.gucheng.statistichelper.database.dao.ItemRecordDao
 import com.gucheng.statistichelper.database.dao.ItemTypeDao
+import com.gucheng.statistichelper.database.entity.ChangeRecord
 import com.gucheng.statistichelper.database.entity.DailyReport
 import com.gucheng.statistichelper.database.entity.ItemRecord
 import com.gucheng.statistichelper.database.entity.ItemType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-@Database(entities = [ItemRecord::class, ItemType::class, DailyReport::class], version = 4, exportSchema = false)
+@Database(
+    entities = [ItemRecord::class, ItemType::class, DailyReport::class, ChangeRecord::class],
+    version = 5,
+    exportSchema = false
+)
 abstract class AccountDatabase : RoomDatabase() {
 
     abstract fun itemRecordDao(): ItemRecordDao
     abstract fun itemTypeDao(): ItemTypeDao
-    abstract fun dailyReportDao() : DailyReportDao
+    abstract fun dailyReportDao(): DailyReportDao
 
     companion object {
         @Volatile
         private var INSTANCE: AccountDatabase? = null
 
-        val MIGARATION_3_4 = object :Migration(3,4) {
+        val MIGARATION_3_4 = object : Migration(3, 4) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE daily_report ADD COLUMN createTime TEXT")
             }
 
+        }
+
+        val MIGARATION_4_5 = object : Migration(4,5) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE TABLE change_record( id INTEGER primary key autoincrement," +
+                        "createTime TEXT ," +
+                        "changeAmount DOUBLE," +
+                        "remark TEXT," +
+                        "typeId INT not null," +
+                        "typeName TEXT NOT NULL" +
+                        ")")
+            }
         }
 
         fun getDatabase(
@@ -43,7 +60,7 @@ abstract class AccountDatabase : RoomDatabase() {
                     AccountDatabase::class.java,
                     "account_database"
                 ).addCallback(AccountDatabaseCallback(scope))
-                    .addMigrations(MIGARATION_3_4).build()
+                    .addMigrations(MIGARATION_3_4, MIGARATION_4_5).build()
                 INSTANCE = instance
                 instance
             }
@@ -59,6 +76,8 @@ abstract class AccountDatabase : RoomDatabase() {
                     }
                 }
             }
+
+
         }
 
         suspend fun populateDatabase(itemRecordDao: ItemRecordDao, itemTypeDao: ItemTypeDao) {
---
+++ app/src/main/java/com/gucheng/statistichelper/database/entity/ChangeRecord.kt
@@ -0,0 +1,18 @@
+package com.gucheng.statistichelper.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.gucheng.statistichelper.Utils
+
+/**
+ * Created on 2021/10/11.
+ */
+@Entity(tableName = "change_record")
+data class ChangeRecord(
+    @PrimaryKey(autoGenerate = true) var id: Int?,
+    var createTime: String? = Utils.timestampToDate(System.currentTimeMillis()),
+    var changeAmount: Double? = 0.0,
+    var remark:String?,
+    var typeId:Int,
+    var typeName:String
+)
